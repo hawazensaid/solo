0f731dd0ea49d3d53ff779866c22c49a1f53caa4
hawazensaid <hawazensaid55@gmail.com>
2025-10-05 12:19:32 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+    let hScore=0
+  
+   let gScore=0
+
+function add1(){
+
+    hScore+=1
+    document.getElementById("home-score").textContent=hScore
+    
+}
+function add2(){
+
+    hScore+=2
+    document.getElementById("home-score").textContent=hScore
+    
+}
+function add3(){
+
+    hScore+=3
+    document.getElementById("home-score").textContent=hScore
+    
+}
+
+function add1g(){
+
+    gScore+=1
+    document.getElementById("guest-score").textContent=gScore
+    
+}
+function add2g(){
+
+    gScore+=2
+    document.getElementById("guest-score").textContent=gScore
+    
+}
+function add3g(){
+
+    gScore+=3
+    document.getElementById("guest-score").textContent=gScore
+    
+}
